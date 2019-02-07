d5fad96918f129286e23e5ee8540d7b9ced9e5e8
Diracdeltafunction <DJJ698@student.bham.ac.uk>
2019-02-07 14:15:49 +0000

    Test

---
+++ src/B1SteppingAction.cc
@@ -0,0 +1,85 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1SteppingAction.cc
+/// \brief Implementation of the B1SteppingAction class
+
+#include "B1SteppingAction.hh"
+#include "B1EventAction.hh"
+#include "B1DetectorConstruction.hh"
+
+#include "G4Step.hh"
+#include "G4Event.hh"
+#include "G4RunManager.hh"
+#include "G4LogicalVolume.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1SteppingAction::B1SteppingAction(B1EventAction* eventAction)
+: G4UserSteppingAction(),
+  fEventAction(eventAction),
+  fScoringVolume(0)
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1SteppingAction::~B1SteppingAction()
+{}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1SteppingAction::UserSteppingAction(const G4Step* step)
+{
+  
+  // get volume of the current step
+  G4LogicalVolume* volume 
+    = step->GetPreStepPoint()->GetTouchableHandle()
+      ->GetVolume()->GetLogicalVolume();
+      
+  // check if we are in scoring volume
+  if (volume->GetName() != "Scatterer" && volume->GetName() != "Absorber") return;
+
+  // collect energy deposited in step
+  // scatterer energy
+  if (volume->GetName() == "Scatterer")
+    {
+      G4double edepStep = step->GetTotalEnergyDeposit();
+      G4double timeScatterer = step->GetTrack()->GetGlobalTime();
+      fEventAction->AddEdepScatterer(edepStep);
+      fEventAction->TimeScatterer(timeScatterer);
+    }
+  // detector energy
+  if (volume->GetName() == "Absorber")
+    {
+      G4double edepStep = step->GetTotalEnergyDeposit();
+      G4double timeDetector = step->GetTrack()->GetGlobalTime();
+      fEventAction->AddEdepDetector(edepStep);
+      fEventAction->TimeDetector(timeDetector);
+    }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
