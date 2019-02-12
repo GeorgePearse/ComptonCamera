bf74629d4fb6260d013b28505ae93c08aedba47e
jpm602 <jpm602@student.bham.ac.uk>
2019-02-12 14:11:06 +0000

    test

---
+++ include/B1DetectorMessenger.hh
@@ -0,0 +1,7 @@
+#ifndef B1DetectorMessenger_h
+#define B1DetectorMessenger_h 1
+
+#include "G4UImessenger.hh"
+#include "globals.hh"
+
+
---
+++ src/B1DetectorMessenger.cc
@@ -0,0 +1,25 @@
+#include "B1DetectorMessenger.hh"
+#include "B1DetectorConstruction.hh"
+
+#include "G4UIdirectory.hh"
+#include "G4UIcmdWithAString.hh"
+#include "G4UIcmdWithoutParameter.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1DetectorMessenger::B1DetectorMessenger(B1DetectorConstruction * det)
+:fDetector(det)
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1DetectorMessenger::~B1DetectorMessenger()
+{
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1DetectorMessenger::SetNewValue(G4UIcommand* command, G4String newValue)
+{
+}
